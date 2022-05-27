858446a8d381eca6297a43da398d9194feb98703
Tyler Gordon <tagordon@uw.edu>
2022-05-27 15:21:14 -0700

    try again

--- gefera/__init__.py
+++ gefera/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ['Orbit', 'System', 'SatelliteOrbit', 'PrimaryOrbit', 'ConfocalOrbit', 'HierarchicalSystem', 'ConfocalSystem']
-from .gefera import SatelliteOrbit, PrimaryOrbit, ConfocalOrbit, HierarchicalSystem, ConfocalSystem
+from .gefera import System, Orbit, SatelliteOrbit, PrimaryOrbit, ConfocalOrbit, HierarchicalSystem, ConfocalSystem
 from .animate import *
