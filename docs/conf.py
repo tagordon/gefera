15c3d711e9d6f4e60345bac684830b8f0e386e8e
Tyler Gordon <tagordon@uw.edu>
2022-05-27 13:05:36 -0700

    try this

--- docs/conf.py
+++ docs/conf.py
@@ -32,8 +32,22 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 
+    'sphinx.ext.coverage', 
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
+    'nbsphinx',
+    'sphinx_rtd_theme']
+
+nbsphinx_prolog = """
+.. raw:: html
+    <style>
+        .nbinput .prompt,
+        .nboutput .prompt {
+        display: none;
+        }
+    </style>
+"""
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
