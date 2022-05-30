54f6b9a8ef4faef294184c6fbe006995a1850e67
Tyler Gordon <tagordon@uw.edu>
2022-05-30 15:50:50 -0700

    try this

--- tests/.ipynb_checkpoints/test_kepler-checkpoint.py
+++ tests/.ipynb_checkpoints/test_kepler-checkpoint.py
@@ -23,9 +23,9 @@ def test_solver():
 def test_impact_conf():
     
     d = tu.random_args_conf()
-    o1 = gf.PrimaryOrbit(d['a1'], d['t1'], d['e1'], d['p1'], d['w1'], d['i1'])
-    o2 = gf.ConfocalOrbit(d['a2'], d['t2'], d['e2'], d['p2'], d['o2'], d['w2'], d['i2'])
-    sys = gf.ConfocalSystem(o1, o2)
+    o1 = gf.orbits.PrimaryOrbit(d['a1'], d['t1'], d['e1'], d['p1'], d['w1'], d['i1'])
+    o2 = gf.orbits.ConfocalOrbit(d['a2'], d['t2'], d['e2'], d['p2'], d['o2'], d['w2'], d['i2'])
+    sys = gf.systems.ConfocalSystem(o1, o2)
     
     t = np.linspace(0, np.max([d['p1'], d['p2']]), 1000)
     (x1, y1, z1), (x2, y2, z2) = sys.coords(t)
@@ -43,9 +43,9 @@ def test_impact_conf():
 def test_impact_hrch():
     
     d = tu.random_args_hrch()
-    o1 = gf.PrimaryOrbit(d['a1'], d['t1'], d['e1'], d['p1'], d['w1'], d['i1'])
-    o2 = gf.SatelliteOrbit(d['a2'], d['t2'], d['e2'], d['p2'], d['o2'], d['w2'], d['i2'], d['m2'])
-    sys = gf.HierarchicalSystem(o1, o2)
+    o1 = gf.orbits.PrimaryOrbit(d['a1'], d['t1'], d['e1'], d['p1'], d['w1'], d['i1'])
+    o2 = gf.orbits.SatelliteOrbit(d['a2'], d['t2'], d['e2'], d['p2'], d['o2'], d['w2'], d['i2'], d['m2'])
+    sys = gf.systems.HierarchicalSystem(o1, o2)
     
     t = np.linspace(0, np.max([d['p1'], d['p2']]), 1000)
     (x1, y1, z1), (x2, y2, z2) = sys.coords(t)
@@ -90,9 +90,9 @@ def test_grad_conf():
         t = np.linspace(0, np.max([d[3], d[9]]), 10)
         dx = 1e-8
 
-        o1 = gf.PrimaryOrbit(d[0], d[1], d[2], d[3], d[4], d[5])
-        o2 = gf.ConfocalOrbit(d[6], d[7], d[8], d[9], d[10], d[11], d[12])
-        sys = gf.ConfocalSystem(o1, o2)
+        o1 = gf.orbits.PrimaryOrbit(d[0], d[1], d[2], d[3], d[4], d[5])
+        o2 = gf.orbits.ConfocalOrbit(d[6], d[7], d[8], d[9], d[10], d[11], d[12])
+        sys = gf.systems.ConfocalSystem(o1, o2)
 
         bp, bpm, theta, dbp, dbpm, dtheta = sys.impacts(t, grad=True)
         dbp_fd = np.zeros_like(dbp)
@@ -101,14 +101,14 @@ def test_grad_conf():
 
         for i in range(len(d)):
             d[i] += dx
-            o1 = gf.PrimaryOrbit(d[0], d[1], d[2], d[3], d[4], d[5])
-            o2 = gf.ConfocalOrbit(d[6], d[7], d[8], d[9], d[10], d[11], d[12])
-            sys_plus = gf.ConfocalSystem(o1, o2)
+            o1 = gf.orbits.PrimaryOrbit(d[0], d[1], d[2], d[3], d[4], d[5])
+            o2 = gf.orbits.ConfocalOrbit(d[6], d[7], d[8], d[9], d[10], d[11], d[12])
+            sys_plus = gf.systems.ConfocalSystem(o1, o2)
     
             d[i] -= 2 * dx
-            o1 = gf.PrimaryOrbit(d[0], d[1], d[2], d[3], d[4], d[5])
-            o2 = gf.ConfocalOrbit(d[6], d[7], d[8], d[9], d[10], d[11], d[12])
-            sys_minus = gf.ConfocalSystem(o1, o2)
+            o1 = gf.orbits.PrimaryOrbit(d[0], d[1], d[2], d[3], d[4], d[5])
+            o2 = gf.orbits.ConfocalOrbit(d[6], d[7], d[8], d[9], d[10], d[11], d[12])
+            sys_minus = gf.systems.ConfocalSystem(o1, o2)
     
             bp_plus, bpm_plus, theta_plus = sys_plus.impacts(t)
             bp_minus, bpm_minus, theta_minus = sys_minus.impacts(t)
@@ -162,14 +162,14 @@ def test_grad_hrch():
 
         for i in range(len(d)):
             d[i] += dx
-            o1 = gf.PrimaryOrbit(d[0], d[1], d[2], d[3], d[4], d[5])
-            o2 = gf.SatelliteOrbit(d[6], d[7], d[8], d[9], d[10], d[11], d[12], d[13])
-            sys_plus = gf.HierarchicalSystem(o1, o2)
+            o1 = gf.orbits.PrimaryOrbit(d[0], d[1], d[2], d[3], d[4], d[5])
+            o2 = gf.orbits.SatelliteOrbit(d[6], d[7], d[8], d[9], d[10], d[11], d[12], d[13])
+            sys_plus = gf.systems.HierarchicalSystem(o1, o2)
     
             d[i] -= 2 * dx
-            o1 = gf.PrimaryOrbit(d[0], d[1], d[2], d[3], d[4], d[5])
-            o2 = gf.SatelliteOrbit(d[6], d[7], d[8], d[9], d[10], d[11], d[12], d[13])
-            sys_minus = gf.HierarchicalSystem(o1, o2)
+            o1 = gf.orbits.PrimaryOrbit(d[0], d[1], d[2], d[3], d[4], d[5])
+            o2 = gf.orbits.SatelliteOrbit(d[6], d[7], d[8], d[9], d[10], d[11], d[12], d[13])
+            sys_minus = gf.systems.HierarchicalSystem(o1, o2)
         
             bp_plus, bpm_plus, theta_plus = sys_plus.impacts(t)
             bp_minus, bpm_minus, theta_minus = sys_minus.impacts(t)
